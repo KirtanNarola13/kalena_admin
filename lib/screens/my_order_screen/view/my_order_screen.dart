924a5e4c8e58fa3cb8e6fc1ab7db3ca3fb840ddc
r31 <padariyavrund725@gmail.com>
2024-06-10 12:11:29 +0530

    Admin completed with group ordred

--- lib/screens/my_order_screen/view/my_order_screen.dart
+++ lib/screens/my_order_screen/view/my_order_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:kalena_admin/utils/helper/firebase_helper/firestore_helper.dart';
 
 class MyOrderScreen extends StatefulWidget {
-  const MyOrderScreen({Key? key}) : super(key: key);
+  MyOrderScreen({Key? key}) : super(key: key);
 
   @override
   State<MyOrderScreen> createState() => _MyOrderScreenState();
@@ -14,8 +14,6 @@ class MyOrderScreen extends StatefulWidget {
 class _MyOrderScreenState extends State<MyOrderScreen> {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
         stream: FirestoreHelper.firestoreHelper.fetchOrders(),
@@ -25,137 +23,27 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
               child: Text(snapshot.error.toString()),
             );
           } else if (snapshot.hasData) {
-            List<QueryDocumentSnapshot<Map<String, dynamic>>> orders =
-                snapshot.data!.docs;
+            Map<String, List<DocumentSnapshot<Map<String, dynamic>>>>
+                groupedOrders = groupOrdersByCustomer(snapshot.data!.docs);
 
-            return ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, index) {
-                var order = orders[index];
-                List<dynamic> products = order['products'];
-                log(order.toString());
-                return Row(
-                  children: [
-                    Expanded(
-                      flex: 5,
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Iterate over products and display their information
-                            for (var product in products)
-                              Container(
-                                alignment: Alignment.center,
-                                margin: const EdgeInsets.all(10),
-                                height: height * 0.32,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey.shade300,
-                                  borderRadius: const BorderRadius.all(
-                                    Radius.circular(
-                                      15,
-                                    ),
-                                  ),
-                                ),
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: Container(
-                                        margin: const EdgeInsets.all(20),
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius: BorderRadius.all(
-                                            Radius.circular(15),
-                                          ),
-                                          image: DecorationImage(
-                                            image: NetworkImage(
-                                                "${product['image']},"),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Container(
-                                        alignment: Alignment.center,
-                                        margin: const EdgeInsets.all(10),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceAround,
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Expanded(
-                                              flex: 2,
-                                              child: Text(
-                                                product['name'],
-                                                style: TextStyle(
-                                                  fontSize: 14,
-                                                ),
-                                              ),
-                                            ),
-                                            Divider(),
-                                            Expanded(
-                                              flex: 2,
-                                              child: Text(
-                                                product['description'],
-                                                style: TextStyle(
-                                                  fontSize: 12,
-                                                ),
-                                              ),
-                                            ),
-                                            Divider(),
-                                            Expanded(
-                                              flex: 2,
-                                              child: Text(
-                                                "${orders[index]['address']}",
-                                                style: TextStyle(
-                                                  fontSize: 12,
-                                                ),
-                                              ),
-                                            ),
-                                            Divider(),
-                                            Expanded(
-                                              child:
-                                                  Text("${product['price']}"),
-                                            ),
-                                            Divider(),
-                                            Expanded(
-                                              child: Row(
-                                                children: [
-                                                  Text("Quantity : "),
-                                                  Text(
-                                                      "${product['quantity']}"),
-                                                ],
-                                              ),
-                                            ),
-                                            Divider(),
-                                            TextButton(
-                                              onPressed: () {
-                                                _completeOrder(
-                                                    order.id,
-                                                    product[
-                                                        'id']); // Assuming product['id'] is the productId
-                                              },
-                                              child: Text("Complete Order"),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+            return ListView(
+              children: groupedOrders.entries.map((entry) {
+                String customerName = entry.key;
+                List<DocumentSnapshot<Map<String, dynamic>>> orders =
+                    entry.value;
+                return Card(
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                  child: ExpansionTile(
+                    title: Text(customerName),
+                    children: orders.map((order) {
+                      return buildOrderTile(order);
+                    }).toList(),
+                  ),
                 );
-              },
+              }).toList(),
             );
           }
-          return const Center(
+          return Center(
             child: CircularProgressIndicator(),
           );
         },
@@ -163,39 +51,87 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
     );
   }
 
-  Future<void> _completeOrder(String orderId, String productId) async {
+  // Group orders by customer name
+  Map<String, List<DocumentSnapshot<Map<String, dynamic>>>>
+      groupOrdersByCustomer(
+          List<DocumentSnapshot<Map<String, dynamic>>> orders) {
+    Map<String, List<DocumentSnapshot<Map<String, dynamic>>>> groupedOrders =
+        {};
+
+    for (var order in orders) {
+      String customerName = order.get('name');
+      if (!groupedOrders.containsKey(customerName)) {
+        groupedOrders[customerName] = [];
+      }
+      groupedOrders[customerName]!.add(order);
+    }
+
+    return groupedOrders;
+  }
+
+  Widget buildOrderTile(DocumentSnapshot<Map<String, dynamic>> order) {
+    List<dynamic> products = order['products'];
+    return ListTile(
+      title: Text('Order ID: ${order.id}'),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          for (var product in products)
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 150,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                color: Colors.grey,
+                width: 2,
+              )),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      child: Image.network(product['image']),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(product['name']),
+                        Text(product['description']),
+                        Text('${product['price']}'),
+                        Text('Quantity: ${product['quantity']}'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+      dense: true,
+      trailing: IconButton(
+        icon: Text('Complete'),
+        onPressed: () {
+          _completeOrder(order.id);
+        },
+      ),
+    );
+  }
+
+  Future<void> _completeOrder(String orderId) async {
     try {
       // Get a reference to the order document using the provided order ID
       DocumentReference orderReference =
           FirebaseFirestore.instance.collection("orders").doc(orderId);
 
-      // Fetch the order document
-      DocumentSnapshot orderSnapshot = await orderReference.get();
+      // Delete the order document
+      await orderReference.delete();
 
-      if (orderSnapshot.exists) {
-        // Get the list of products from the order
-        List<dynamic> products = orderSnapshot.get('products');
-
-        // Find the index of the product in the 'products' array
-        int index =
-            products.indexWhere((product) => product['id'] == productId);
-
-        if (index != -1) {
-          // Remove the product from the 'products' array
-          products.removeAt(index);
-
-          // Update the 'products' field in the order document
-          await orderReference.update({'products': products});
-
-          log("Product with ID: $productId removed from order with ID: $orderId");
-        } else {
-          log("Product with ID: $productId not found in order with ID: $orderId");
-        }
-      } else {
-        log("Order with ID: $orderId does not exist.");
-      }
+      log("Order with ID: $orderId deleted successfully");
     } catch (e) {
-      log("Error removing product from order: $e");
+      log("Error deleting order: $e");
     }
   }
 }
